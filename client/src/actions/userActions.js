8856672064c320de777bfff8dc918ff190c200d9
brent <brent.wish@gmail.com>
2018-11-15 22:13:28 -0800

    Adds login and signup modals to Header.js component

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -18,20 +18,78 @@ export const login = () => {
   }
 }
 
-export const changeEmailText = text => {
-  return (dispatch) => {
-    dispatch({
-      type: 'CHANGE_EMAIL_TEXT',
-      text
+export const signup = () => {
+  return (dispatch, getState) => {
+    //get the user's login info from the store
+    const state = getState().userReducer;
+    const user = { email: state.emailText, password: state.passwordText };
+    //ask the server if the credentials are ok
+    axios.post("http://ericw-appliance-rdqrpqhkdd.dynamic-m.com:3000/login", { user })
+    .then(res => {
+      dispatch({ type: 'LOGIN', res });
+    }).catch(err => {
+      dispatch({ type: 'LOGIN_FAILED', err });
     });
   }
 }
 
-export const changePasswordText = text => {
-  return (dispatch) => {
-    dispatch({
-      type: 'CHANGE_PASSWORD_TEXT',
-      text
-    });
+export const changeLoginUsername = text => {
+  return {
+    type: 'CHANGE_LOGIN_USERNAME',
+    text
   }
+};
+
+export const changeLoginPassword = text => {
+  return {
+    type: 'CHANGE_LOGIN_PASSWORD',
+    text
+  }
+};
+
+export const changeSignupUsername = text => {
+  return {
+    type: 'CHANGE_SIGNUP_USERNAME',
+    text
+  }
+};
+export const changeSignupEmail = text => {
+  return {
+    type: 'CHANGE_SIGNUP_EMAIL',
+    text
+  }
+};
+export const changeSignupPassword = text => {
+  return {
+    type: 'CHANGE_SIGNUP_PASSWORD',
+    text
+  }
+};
+export const changeSignupConfirmPassword = text => {
+  return {
+    type: 'CHANGE_SIGNUP_CONFIRM_PASSWORD',
+    text
+  }
+};
+
+export const openLoginModal = () => {
+  return {
+    type: 'OPEN_LOGIN_MODAL',
+  };
+}
+export const closeLoginModal = () => {
+  return {
+    type: 'CLOSE_LOGIN_MODAL',
+  };
+}
+
+export const openSignupModal = () => {
+  return {
+    type: 'OPEN_SIGNUP_MODAL',
+  };
+}
+export const closeSignupModal = () => {
+  return {
+    type: 'CLOSE_SIGNUP_MODAL',
+  };
 }
--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -3,10 +3,29 @@
 */
 export default (state = { emailText: "", passwordText: "" }, action) => {
   switch (action.type) {
-    case 'CHANGE_EMAIL_TEXT':
-      return { ...state, emailText: action.text }
-    case 'CHANGE_PASSWORD_TEXT':
-      return { ...state, passwordText: action.text }
+    case 'CHANGE_LOGIN_USERNAME':
+      return { ...state, loginUsername: action.text }
+    case 'CHANGE_LOGIN_PASSWORD':
+      return { ...state, loginPassword: action.text }
+
+    case 'CHANGE_SIGNUP_USERNAME':
+      return { ...state, signupUsername: action.text }
+    case 'CHANGE_SIGNUP_EMAIL':
+      return { ...state, signupEmail: action.text }
+    case 'CHANGE_SIGNUP_PASSWORD':
+      return { ...state, signupPassword: action.text }
+    case 'CHANGE_SIGNUP_CONFIRM_PASSWORD':
+      return { ...state, signupConfirmPassword: action.text }
+
+    case 'OPEN_LOGIN_MODAL':
+      return { ...state, isLoginModalOpen: true }
+    case 'CLOSE_LOGIN_MODAL':
+      return { ...state, isLoginModalOpen: false }
+    case 'OPEN_SIGNUP_MODAL':
+      return { ...state, isSignupModalOpen: true }
+    case 'CLOSE_SIGNUP_MODAL':
+      return { ...state, isSignupModalOpen: false }
+
     default:
       return state
   }
