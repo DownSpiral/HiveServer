00b9d46f08d2db225e702374f5a5c8d882956e10
DownSpiral <eric.d.wishart@gmail.com>
2018-11-15 21:48:46 -0800

    Add user/ship relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   devise :registerable, :database_authenticatable,
          :jwt_authenticatable,
          jwt_revocation_strategy: JWTBlacklist
+
+  has_many :ships, dependent: :destroy
 end
